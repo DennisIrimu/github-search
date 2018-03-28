b2cc9b53fecd2974f18e040092cbb161b18a5442
dnyt <lyricaldnyt@gmail.com>
2018-03-28 07:17:06 +0300

    create git link functions

--- src/app/github/github.service.ts
+++ src/app/github/github.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@angular/core';
-
+import { Http} from '@angular/http';
+import 'rxjs/add/operator/map';
 @Injectable()
 export class GithubService {
-
-  constructor() { }
-
-}
+  private username: string;
+  private clientid='537865c510c4d1068497';
+  private clientsecret='1210045d76ea04fec81298f7484351b1afda89ff';
+  constructor(private http:Http)
+  getUserInfo(){
+     return this.http.get("https://api.github.com/users/" + this.username + "?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+     .map(res => res.json());
+   }
+   getUserRepos(){
+     return this.http.get("https://api.github.com/users/" + this.username + "/repos?client_id=" + this.clientid + "&client_secret=" + this.clientsecret)
+     .map(res => res.json());
+   }
+   updateUser(username:string){
+     this.username = username;
+   }
+   }
